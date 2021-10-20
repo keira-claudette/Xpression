8feb533f44c32024ab15255e9e8d356f6c7d89ce
Claudette Mokeira <68082012+keira-claudette@users.noreply.github.com>
2021-10-20 06:21:09 +0300

    added grid css for search results

---
+++ static/sctipts/main.js
@@ -0,0 +1,17 @@
+$(function () {
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.giphy.com/v1/gifs/trending?api_key=x91jNWIe6d8y2vIH4zrWtQ2dOqgpcsQD&limit=25&rating=g',
+        crossDomain: true,
+        headers: {
+            "accept": "application/json"
+        },
+        success: function(data) {
+            /*console.log('success', data);*/
+            $.each(data.data, function(indx, gif) {
+                $('div#row1').append('<div class="col"><img src="' + gif.images.fixed_width.url + '"</div>');
+            });
+        }
+    });
+/*Implement here search button*/
+});
