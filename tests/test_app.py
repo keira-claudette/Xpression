690a88abc7fe8a6440f7f7fd4d85ee1e36a14cb9
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-11-07 08:27:00 +0300

    testing routes

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,21 +1,29 @@
-from app import *
-import unittest
+try:
+    from app import app
+    import unittest
 
+except Exception as e:
+    print("Some Modules are Missing {}".format(e))
 
-class TestUser(unittest.TestCase):
+class FlaskTest(unittest.TestCase):
+        # check for response 200
+        def test_index(self):
+            tester = app.test_client(self)
+            response = tester.get("/")
+            statuscode = response.status_code
+            self.assertEqual(statuscode, 200)
 
-    def test_attrbuite(self):
-       self.assertTrue(hasattr(User,"id"))
-       self.assertTrue(hasattr(User, "username"))
-       self.assertTrue(hasattr(User, "email"))
-       self.assertTrue(hasattr(User, "password"))
+        # check if content return application Json
+        def test_index_content(self):
+            tester = app.test_client(self)
+            response = tester.get("/")
+            self.assertEqual(response.content_type, "text/html; charset=utf-8")
 
+        # check for Data returned
+        def test_index_data(self):
+                tester = app.test_client(self)
+                response = tester.get("/")
+                self.assertTrue(b'html' in response.data)
 
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    unittest.main()
